42004eda521f13e5116594ca98c0e2aedefec940
AGB <alexgogb@gmail.com>
2026-03-25 11:54:10 +0100

    Fix LCD struct definition

--- libraries/LCD_1602_driver.h
+++ libraries/LCD_1602_driver.h
@@ -12,8 +12,6 @@ typedef struct {
     uint8_t RS_pin;
     uint8_t RW_pin;
     uint8_t E_pin;
-    uint8_t D0_pin, D1_pin, D2_pin, D3_pin,
-            D4_pin, D5_pin, D6_pin, D7_pin;
 } LCD_1602;
 
 void LCD_init(LCD_1602 lcd);
