ef9440d2c96b7866c1df81a988ae802778645077
AGB <alexgogb@gmail.com>
2026-03-25 12:15:14 +0100

    Add new definitions for LCD library

--- libraries/LCD_1602_driver.h
+++ libraries/LCD_1602_driver.h
@@ -8,10 +8,18 @@
 #define MODE_8_BIT 8
 
 typedef struct {
+    // User-defined
     uint8_t mode;
     uint8_t RS_pin;
     uint8_t RW_pin;
     uint8_t E_pin;
+
+    // Useful for the developer
+    uint8_t line;
 } LCD_1602;
 
 void LCD_init(LCD_1602 *lcd, uint8_t mode, uint8_t rs_pin, uint8_t rw_pin, uint8_t e_pin);
+void LCD_write_character(LCD_1602 lcd, uint8_t character);
+void LCD_write_line(LCD_1602 lcd, uint8_t *string);
+void LCD_change_line(LCD_1602 lcd);
+void LCD_clear(LCD_1602 lcd);
