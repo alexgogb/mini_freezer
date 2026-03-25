9ae4aa27cf878b4ed118c101d78ffc66e4e5e5b1
AGB <alexgogb@gmail.com>
2026-03-25 11:54:31 +0100

    Fix SR595 struct definition and initialize function

--- libraries/shift_register_595_driver.h
+++ libraries/shift_register_595_driver.h
@@ -5,8 +5,6 @@
 #include <stdint.h>
 
 typedef struct {
-    int8_t QA_pin, QB_pin, QC_pin, QD_pin,
-            QE_pin, QF_pin, QG_pin, QH_pin, QHp_pin;
     int8_t SER_pin;
     int8_t OE_pin;
     int8_t RCLK_pin;
@@ -14,5 +12,5 @@ typedef struct {
     int8_t SRCLR_pin;
 } shift_register_595;
 
-void shift_register_init(shift_register_595 *reg, int8_t *data_pins, int8_t ser_pin,
+void shift_register_init(shift_register_595 *reg, int8_t ser_pin,
                          int8_t oe_pin, int8_t rclk_pin, int8_t srclk_pin, int8_t srclr_pin);
