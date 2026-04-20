beff8218e09d5566df256ecf68086367d6ac507c
AGB <alexgogb@gmail.com>
2026-04-20 17:54:49 +0200

    Add Peltier control configuration and example in main

--- main/main.c
+++ main/main.c
@@ -18,12 +18,18 @@
 #define SRCLK 6
 #define AUDIO 7
 #define DOOR_WARNING 10
+#define PELTIER 11
+
+#define BUTTON_DEBOUNCE_COUNT 5
 
 #define LEDC_DUTY_RESOLUTION LEDC_TIMER_10_BIT  // (0-1023)
 #define LEDC_AUDIO_CHANNEL LEDC_CHANNEL_0
-#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_AUDIO_TIMER LEDC_TIMER_0
+#define LEDC_PELTIER_CHANNEL LEDC_CHANNEL_1
+#define LEDC_PELTIER_TIMER LEDC_TIMER_2
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_FREQUENCY 1000              // 1 kHz
+#define LEDC_AUDIO_FREQUENCY 1000 // 1 kHz
+#define LEDC_PELTIER_FREQUENCY 4000
 
 void initial_config();
 void door_open_safety_system();
@@ -35,6 +41,9 @@ audio_device ad;
 esp_timer_handle_t timer;
 volatile uint8_t warning_on = 0;
 volatile uint8_t door_warning = 0;
+uint8_t button_counter = 0;
+uint8_t button_last_read = 1;
+uint8_t button_state = 1;
 
 void app_main(void) {
     initial_config();
@@ -68,17 +77,55 @@ void app_main(void) {
     //     vTaskDelay(pdMS_TO_TICKS(10));
     // }
 
-    esp_timer_start_once(timer, 10 * 1000 * 1000);
+    // esp_timer_start_once(timer, 10 * 1000 * 1000);
+
+    // while (1) {
+    //     if (door_warning) {
+    //         gpio_set_level(SER, 1);
+    //         door_warning = 0;
+    //         vTaskDelay(pdMS_TO_TICKS(2000));
+    //         gpio_set_level(SER, 0);
+    //         esp_timer_stop(timer);
+    //         esp_timer_start_once(timer, 5 * 1000 * 1000);
+    //     }
+    //     vTaskDelay(pdMS_TO_TICKS(10));
+    // }
+
+    uint8_t peltier_mode = 0;
+    uint8_t current;
 
     while (1) {
-        if (door_warning) {
-            gpio_set_level(SER, 1);
-            door_warning = 0;
-            vTaskDelay(pdMS_TO_TICKS(2000));
-            gpio_set_level(SER, 0);
-            esp_timer_stop(timer);
-            esp_timer_start_once(timer, 5 * 1000 * 1000);
+        current = gpio_get_level(10);
+
+        if (current == button_last_read) {
+            button_counter++;
+
+            if (button_counter >= BUTTON_DEBOUNCE_COUNT) {
+                if (button_state != current) {
+                    button_state = current;
+
+                    if (button_state) {
+                        if (peltier_mode < 4) {
+                            peltier_mode++;
+                        } else {
+                            peltier_mode = 0;
+                        }
+
+                        if (peltier_mode != 0) {
+                            ledc_set_duty(LEDC_MODE, LEDC_PELTIER_CHANNEL, 1023 / peltier_mode);
+                        } else {
+                            ledc_set_duty(LEDC_MODE, LEDC_PELTIER_CHANNEL, 0);
+                        }
+
+                        ledc_update_duty(LEDC_MODE, LEDC_PELTIER_CHANNEL);
+                    }
+                }
+            }
+        } else {
+            button_counter = 0;
         }
+
+        button_last_read = current;
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
@@ -96,28 +143,45 @@ void initial_config() {
     gpio_config_t io_config_in = {
         .pin_bit_mask = (1ULL << DOOR_WARNING),
         .mode = GPIO_MODE_INPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE,
     };
     gpio_config(&io_config_in);
 
-    ledc_timer_config_t ledc_timer = {
+    ledc_timer_config_t ledc_audio_timer = {
         .speed_mode = LEDC_MODE,
         .duty_resolution = LEDC_DUTY_RESOLUTION,
-        .timer_num = LEDC_TIMER,
-        .freq_hz = LEDC_FREQUENCY
+        .timer_num = LEDC_AUDIO_TIMER,
+        .freq_hz = LEDC_AUDIO_FREQUENCY
     };
-    ledc_timer_config(&ledc_timer);
+    ledc_timer_config(&ledc_audio_timer);
 
-    ledc_channel_config_t ledc_channel = {
+    ledc_channel_config_t ledc_audio_channel = {
         .gpio_num = AUDIO,
         .speed_mode = LEDC_MODE,
         .channel = LEDC_AUDIO_CHANNEL,
-        .timer_sel = LEDC_TIMER,
+        .timer_sel = LEDC_AUDIO_TIMER,
         .duty = 0
     };
-    ledc_channel_config(&ledc_channel);
+    ledc_channel_config(&ledc_audio_channel);
+
+    ledc_timer_config_t ledc_peltier_timer = {
+        .speed_mode = LEDC_MODE,
+        .duty_resolution = LEDC_DUTY_RESOLUTION,
+        .timer_num = LEDC_PELTIER_TIMER,
+        .freq_hz = LEDC_PELTIER_FREQUENCY
+    };
+    ledc_timer_config(&ledc_peltier_timer);
+
+    ledc_channel_config_t ledc_peltier_channel = {
+        .gpio_num = PELTIER,
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_PELTIER_CHANNEL,
+        .timer_sel = LEDC_PELTIER_TIMER,
+        .duty = 0
+    };
+    ledc_channel_config(&ledc_peltier_channel);
 
     esp_timer_create_args_t timer_args = {
         .callback = &door_open_safety_system,
